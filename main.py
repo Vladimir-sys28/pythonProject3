38e2d160781f4a8e6851952da1772a7389196b69
Vladimir-sys28 <75503221+Vladimir-sys28@users.noreply.github.com>
2021-06-02 21:55:03 +0300

    i change my code in github
    change code in github

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# I change code
+# I change code in pycharm
+# I change code in github
